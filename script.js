a7b104fb17a56036804bbbef9bc34e876bbdf32a
Sean <seanpoulter@hotmail.com>
2022-08-14 14:13:24 -0400

    Add click event for X and O in row

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-(function generateGameboard() {
-    const gameboard = document.querySelectorAll('td');
-    const gameboardCells = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let gameboardArray = Array.from(gameboard);
+const getTable = document.createElement('TABLE');
+let clickCounter = 0;
 
-    for (i = 0; i < gameboardCells.length; i++) {
-        gameboardArray[i].textContent = gameboardCells[i]
+(function makeGameboard() {
+    document.body.append(getTable);
+    const makeTBody = getTable.createTBody();
+    const makeRow = makeTBody.insertRow();
+
+    for (i = 0; i < 9; i++) {
+        makeRow.insertCell(i)
     }
 })();
+
+getTable.addEventListener('click', (e) => {
+    clickCounter++
+
+    console.log(clickCounter)
+    if (clickCounter = 0 || clickCounter % 2) {
+        e.target.textContent = "X"
+    }
+    else {
+        e.target.textContent = "O"
+
+    }
+})
