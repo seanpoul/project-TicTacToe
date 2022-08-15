2ab48ede46d0e761ba5a67ce91a2832e76a92438
Sean <seanpoulter@hotmail.com>
2022-08-14 20:39:51 -0400

    Click for X and O works properly.

--- script.js
+++ script.js
@@ -2,35 +2,48 @@ const getTable = document.createElement('TABLE');
 let clickCounter = 0;
 let clickedCell;
 let winConditionsArray = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-    (function makeGameboard() {
-        document.body.append(getTable);
-        const makeTBody = getTable.createTBody();
-        const makeRow = makeTBody.insertRow();
+(function makeGameboard() {
+    document.body.append(getTable);
+    const makeTBody = getTable.createTBody();
+    const makeRow = makeTBody.insertRow();
 
-        for (i = 0; i < 9; i++) {
-            makeRow.insertCell(i)
-        }
-    })();
+    for (i = 0; i < 9; i++) {
+        makeRow.insertCell(i)
+    }
+})();
+
+console.log(clickedCell)
 
 getTable.addEventListener('click', (clickedCell) => {
     clickCounter++
-    // console.log(clickedCell.target.cellIndex)
-    // console.log(clickCounter)
-    if (clickCounter = 0 || clickCounter % 2) {
-        clickedCell.target.textContent = "X"
+    if (clickCounter === 0 || clickCounter % 2) {
+        if (clickedCell.target.textContent === "O" || clickedCell.target.textContent === "X") {
+            clickCounter--
+            return
+        }
+        else if (typeof clickedCell.target.textContent !== 'undefined') {
+            clickedCell.target.textContent = "X";
+        }
     }
     else {
-        clickedCell.target.textContent = "O"
-    }
+        if (clickedCell.target.textContent === "O" || clickedCell.target.textContent === "X") {
+            clickCounter--
+            return
+        }
+        else if (typeof clickedCell.target.textContent !== 'undefined') {
+            clickedCell.target.textContent = "O";
+        }
 
+    }
     return clickedCell
 })
 
 
 function checkWinner(choiceOne, choiceTwo, choiceThree) {
-    // plus or minus 3 from cell index. check textcontent for matches
+
+
 
     console.log(clickedCell)
 
